c6da51096f30432862543d38a4a938f1c17917f0
Ismail Tosun <ismailtosuntnyl@gmail.com>
2020-12-05 23:40:48 +0300

    ss

---
+++ src/main/java/com/sedmelluq/discord/lavaplayer/demo/jda/Token.java
@@ -0,0 +1,27 @@
+package com.sedmelluq.discord.lavaplayer.demo.jda;
+
+public class Token {
+    private static final String TOKEN="Nzc1MzUwMDM3NjQyMjE1NDQ1.X6lC_g.V9_zd5U2zupjuTLNc1uLDJAmQsw";
+    private static final DSes[] dsesArray={
+            new DSes(6,"https://soundcloud.com/ismail-tosun-354492633/d1/s-zMoJBAA4EAY?in=ismail-tosun-354492633/sets/dbot/s-nIKhuSChtIg"),
+            new DSes(4,"https://soundcloud.com/ismail-tosun-354492633/d2/s-fIAirhE67UZ?in=ismail-tosun-354492633/sets/dbot/s-nIKhuSChtIg"),
+            new DSes(6,"https://soundcloud.com/ismail-tosun-354492633/d3/s-GO8hfuAzPhY?in=ismail-tosun-354492633/sets/dbot/s-nIKhuSChtIg"),
+            new DSes(11,"https://soundcloud.com/ismail-tosun-354492633/d4/s-0cqhgeki92A?in=ismail-tosun-354492633/sets/dbot/s-nIKhuSChtIg"),
+            new DSes(6,"https://soundcloud.com/ismail-tosun-354492633/d5/s-P8woN9dZgic?in=ismail-tosun-354492633/sets/dbot/s-nIKhuSChtIg"),
+            new DSes(6,"https://soundcloud.com/ismail-tosun-354492633/d6-1/s-iAV5AF0IofR"),
+            new DSes(4,"https://soundcloud.com/ismail-tosun-354492633/secret-discord/s-CqFAv8YictQ"),
+            new DSes(8,"https://soundcloud.com/ismail-tosun-354492633/desktop-20201119-01251807dvr-trim-online-audio-convertercom/s-q6bNNzWbUXp"),
+            new DSes(5,"https://soundcloud.com/ismail-tosun-354492633/desktop-20201121-01531703dvr-trim-online-audio-convertercom/s-ot9Df3iI0Af"),
+            new DSes(4,"https://soundcloud.com/ismail-tosun-354492633/desktop-20201121-02124405dvr-trim-online-audio-convertercom/s-YrRgtZf67n6"),
+            new DSes(6,"https://soundcloud.com/ismail-tosun-354492633/desktop-20201121-02153706dvr-trim-online-audio-convertercom/s-5KhYz73LO2B")
+    };
+
+
+    public static String getTOKEN() {
+        return TOKEN;
+    }
+
+    public static DSes[] getDsesArray() {
+        return dsesArray;
+    }
+}
